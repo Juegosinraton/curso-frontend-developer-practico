0baac5f28e4a0e81fc438822f3bfdf6494997ae3
juegosinraton <yesus_17718716@hotmail.com>
2022-12-01 13:28:36 -0600

    Agregue navbar en version de mobil

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let email = document.querySelector('.navbar-email');
 const cuentaOrdenes = document.querySelector('.desktop-menu');
+const menuHam = document.querySelector('.menuHam');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-email.addEventListener('click', toogleCuentaOrdenes);
+email.addEventListener('click', toggleCuentaOrdenes);
+menuHam.addEventListener('click', toggleMenuHam);
 
-function toogleCuentaOrdenes(){
+function toggleCuentaOrdenes(){
     cuentaOrdenes.classList.toggle('inactive');
 } 
+function toggleMenuHam(){
+    mobileMenu.classList.toggle('inactive');
+}
