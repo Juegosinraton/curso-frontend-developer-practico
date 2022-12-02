9497a628d0706ab547a7f137996a7286361fc34c
juegosinraton <yesus_17718716@hotmail.com>
2022-12-01 18:05:19 -0600

    condicionales para cerrar usuario, carro y nav

--- script.js
+++ script.js
@@ -2,13 +2,33 @@ let email = document.querySelector('.navbar-email');
 const cuentaOrdenes = document.querySelector('.desktop-menu');
 const menuHam = document.querySelector('.menuHam');
 const mobileMenu = document.querySelector('.mobile-menu');
+const carrito = document.querySelector('.carrito');
+const itemsCarro = document.querySelector('.product-detail');
 
 email.addEventListener('click', toggleCuentaOrdenes);
 menuHam.addEventListener('click', toggleMenuHam);
+carrito.addEventListener('click', toggleCarrito);
 
 function toggleCuentaOrdenes(){
     cuentaOrdenes.classList.toggle('inactive');
+    if(!cuentaOrdenes.classList.contains('inactive')){
+        mobileMenu.classList.add('inactive');
+        itemsCarro.classList.add('inactive');
+    }
 } 
+
 function toggleMenuHam(){
     mobileMenu.classList.toggle('inactive');
+    if(!mobileMenu.classList.contains('inactive')){
+        itemsCarro.classList.add('inactive');
+        cuentaOrdenes.classList.add('inactive');
+    }
+}
+
+function toggleCarrito(){
+    itemsCarro.classList.toggle('inactive');
+    if(!itemsCarro.classList.contains('inactive')){
+        cuentaOrdenes.classList.add('inactive');
+        mobileMenu.classList.add('inactive');
+    }
 }
